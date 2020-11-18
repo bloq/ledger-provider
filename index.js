c6d92c639cb0a4e38c8389a25e1c6695895bcde2
Manoj <patidarmanoj@gmail.com>
2020-11-18 11:49:36 -0800

    debug log

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TransportNodeHid = require("@ledgerhq/hw-transport-node-hid").default;
 const FetchSubprovider = require("web3-provider-engine/subproviders/fetch");
 const ProviderEngine = require("web3-provider-engine");
 const createLedgerSubprovider = require("@ledgerhq/web3-subprovider").default;
-
+const { listen } = require("@ledgerhq/logs");
 const defaultOptions = {
   networkId: 1,
   paths: ["44'/60'/0'/0/0"],
@@ -15,10 +15,10 @@ function LedgerWalletProvider(
 ) {
   const getTransport = async () => {
     return TransportNodeHid.create(5000).then((transport) => {
-      transport.setDebugMode(true);
       return transport;
     });
   };
+  listen(log => console.log(log))
   const ledger = createLedgerSubprovider(getTransport, options);
   this.engine = new ProviderEngine();
   this.engine.addProvider(ledger);
